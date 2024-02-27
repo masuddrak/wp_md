10654b0cc5345c2bf4acba633886ac96f3324340
masud <masudranaacb@gmail.com>
2024-02-27 14:09:46 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Moshiur - Bootstrap Responsive Personal Portfolio Template</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/jquery.fancybox.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/responsive.css">
+  <link rel="stylesheet" href="css/animate.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <?php wp_head(); ?>
+</head>
